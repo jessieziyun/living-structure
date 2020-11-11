98517d4aae0a0b2141933c00ed4312f2f04eda45
Jessie Zhang <jessiezhang98@gmail.com>
2020-11-11 16:43:05 +0000

    added ui and archive

---
+++ public/index.js
@@ -0,0 +1,376 @@
+import * as THREE from '../three/three.module.js';
+import { MarchingCubes } from '../three/MarchingCubes.js';
+
+// SCENE SETUP
+let container;
+let camera, scene, renderer;
+let light, pointLight, ambientLight;
+const lightXPos = 1.5;
+const lightYPos = 0.5;
+const lightZPos = 0.8;
+
+// MARCHING CUBES
+const resolution = 48;
+let effect;
+
+// HAND TRACKING
+const video = document.getElementById("video");
+let videoWidth, videoHeight;
+let model = null;
+const modelParams = {
+    flipHorizontal: true, // flip for video  
+    maxNumBoxes: 1, // maximum number of boxes to detect
+    iouThreshold: 0.5, // ioU threshold for non-max suppression
+    scoreThreshold: 0.6, // confidence threshold for predictions.
+}
+
+// SOCKET.IO
+const socket = io();
+let hands = [];
+
+// ARCHIVE
+let active = true;
+
+// RUN CODE
+main();
+
+// LANDING PAGE
+function main() {
+
+    // display number of connections
+    socket.on('numclients', data => {
+        $("#numclients").text(`>number of connections: ${data}`);
+    });
+
+    // load archive of screenshots
+    socket.on('allscreenshots', data => {
+        console.log("received archived screenshots");
+        for (let i = 0; i < data.length; i++) {
+            const newdiv = $( "<div class='captioned-image left'></div>" );
+            newdiv.append($("<img>", {
+                class: "archive-image",
+                src: data[i].img
+            }), `<p>>${data[i].time}</p>`)
+            for (let j = 0; j < data[i].people.length; j ++) {
+                newdiv.append(`<p> ${data[i].people[j].name}, ${data[i].people[j].location}</p>`);
+            }
+            $("#gallery").prepend(newdiv);
+        }
+    })
+
+    // landing page
+    $("#start-button").click(() => {
+        $("#landing-page").addClass("fadeOut");
+        setTimeout(() => {
+            $("#landing-page").addClass("hidden");
+          }, 500);
+        $("#input-text").addClass("fadeIn");
+    });
+    
+    // input name and location
+    $("#enter-button").click(() => {
+        getUserInput();   
+        $("#input-text").removeClass("fadeIn");
+        $("#input-text").addClass("fadeOut");
+        setTimeout(() => {
+            $("#input-text").addClass("hidden");
+          }, 500);
+        $("#loading-screen").addClass("fadeIn");
+
+        // start
+        init();
+    })
+}
+
+function getUserInput(){
+    const name = $("#username").val();
+    const location = $("#location").val();
+    const user_info = {name, location};
+    socket.emit('userinfo', user_info);
+}
+
+// INITIALISE HANDTRACK.JS THEN CALL MAIN()
+function init() {
+
+    handTrack.load(modelParams).then(lmodel => {
+        model = lmodel
+        handTrack.startVideo(video).then(status => {
+            console.log("video started", "handtrack status: " + status);
+            videoWidth = video.width;
+            videoHeight = video.height;
+            if (status) {
+                startThree();
+                handleDomElems();
+            }
+        });
+    });
+}
+
+// CREATE THREE.JS SCENE AND LISTEN FOR SERVER DATA
+function startThree() {
+
+    container = document.getElementById('scene-container');
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x0d0d0c);
+
+    createCamera();
+    createLights();
+    createRenderer();
+
+    createMetaballs();
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    animate();
+
+    // display number of connections
+    socket.on('clientconnected', data => {
+        $("#numclients").text(`>number of connections: ${data.num}`);
+        $("#connections").append(`<p>>${data.name} connected from ${data.location}</p>`);
+        $("#connection-events").animate({ scrollTop: $("#connections").height() }, 800);
+    });
+
+    socket.on('newscreenshot', data => {
+        console.log("new screenshot received");
+        const newdiv = $( "<div class='captioned-image left'></div>" );
+        newdiv.append($("<img>", {
+            class: "archive-image",
+            src: data.img
+        }), `<p>>${data.time}</p>`)
+        for (let i = 0; i < data.people.length; i ++) {
+            newdiv.append(`<p> ${data.people[i].name}, ${data.people[i].location}</p>`);
+        }
+        $("#gallery").prepend(newdiv);
+    })
+
+    socket.on('clientdisconnected', data => {
+        $("#numclients").text(`>number of connections: ${data.num}`);
+        $("#connections").append(`<p>>${data.name} disconnected from ${data.location}</p>`);
+        $("#connection-events").animate({ scrollTop: $("#connections").height() }, 800);
+    });
+
+    // receive all connected clients' hand position data
+    socket.on('moving', data => {
+        hands = data;
+    });
+}
+
+// ANIMATION LOOP
+function animate() {
+    runDetection();
+    render();
+    if (active) requestAnimationFrame(animate);
+}
+
+// DETECT HAND POSITION AND SEND TO SERVER
+function runDetection() {
+    model.detect(video).then(predictions => {
+        if (predictions[0]) {
+
+            // detect + normalise x coord
+            const min_x = predictions[0].bbox[0];
+            const bbox_width = predictions[0].bbox[2];
+            const hand_x = min_x + bbox_width / 2;
+            const n_hand_x = hand_x / videoWidth;
+
+            // detect + normalise y coord
+            const min_y =  - predictions[0].bbox[1];
+            const bbox_height = - predictions[0].bbox[3];
+            const hand_y = min_y + bbox_height / 2 + videoHeight;
+            const n_hand_y = hand_y / videoHeight;
+
+            // send to server
+            const user_hand = {
+                x: n_hand_x,
+                y: n_hand_y
+            }
+            socket.emit('handmoved', user_hand);
+        }
+    });
+}
+
+// RENDER
+function render() {
+    updateCubes(effect, hands);
+    renderer.render(scene, camera);
+}
+
+// DISPLAY METABALLS AT CONNECTED CLIENTS' HAND POSITIONS
+function updateCubes(object, hand) {
+    object.reset();
+    const subtract = 12;
+    const strength = 0.8;
+    for (let i = 0; i < hand.length; i++) {
+        const ballx = hand[i].x; 
+        const bally = hand[i].y;
+        const ballz = 0.75;
+        object.addBall(ballx, bally, ballz, strength, subtract);
+    }
+}
+
+// CREATE MARCHING CUBES METABALLS AND ADD TO SCENE
+function createMetaballs() {
+    const material = new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            specular: 0xffffff,
+            shininess: 2,
+            vertexColors: true
+    });
+    effect = new MarchingCubes(resolution, material, false, true);
+    effect.position.set(0, 0, 0);
+    effect.scale.set(700, 700, 700);
+    effect.isolation = 150;
+    scene.add(effect);
+}
+
+// CREATE CAMERA
+function createCamera(){
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(0, 0, 1500);
+}
+
+// CREATE LIGHTS
+function createLights(){
+    light = new THREE.DirectionalLight(0xebebae);
+    light.position.set(lightXPos, lightYPos, lightZPos);
+    light.position.normalize();
+    scene.add(light);
+
+    pointLight = new THREE.PointLight(0xfff7d6);
+    pointLight.position.set(0, 0, 75);
+    scene.add(pointLight);
+
+    ambientLight = new THREE.AmbientLight(0x080808);
+    scene.add(ambientLight);
+}
+
+// CREATE RENDERER
+function createRenderer(){
+    renderer = new THREE.WebGLRenderer();
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+// UPDATE IF WINDOW RESIZED
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// FADE OUT THE LOADING SCREEN AND HANDLE ALL CLICKS
+function handleDomElems() {
+    
+    // fade out loading screen
+    $("#loading-screen").removeClass("fadeIn");
+    $("#loading-screen").addClass("fadeOut");
+    setTimeout(() => {
+        $("#loading-screen").addClass("hidden");
+      }, 500);
+    
+    // fade in main
+    $("#main").addClass("fadeIn");
+
+    // toggle about
+    $("#about").click(() => {
+        // $("#about").removeClass("fadeIn");
+        $("#exit-about").css('display', 'block');
+        $("#about-dropdown").css('display', 'block');
+        $("#about").css({'display': 'none', 'opacity': '1'});
+    });
+    $("#exit-about").click(() => {
+        $("#about-dropdown").css('display', 'none');
+        $("#about").css('display', 'block');
+        $("#exit-about").css('display', 'none');
+    });
+
+    // toggle archive
+    $("#enter-archive").click(() => {
+        $("#archive").css('display', 'block');
+        active = false;
+    });
+    $("#exit-archive").click(() => {
+        $("#archive").css('display', 'none');
+        active = true;
+        animate();
+    });
+    
+    let dataURL;
+
+    // take screenshot
+    $("#take-screenshot").click(() => {
+
+        // display screenshot and save options
+        $("#screenshot").empty();
+        render();
+        dataURL = renderer.domElement.toDataURL('image/png');
+        $("#screenshot").append(
+            $("<img>", {
+                class: "small-image",
+                src: dataURL
+            })
+        );
+        
+        $("#save-screenshot").css('display', 'block');
+        active = false;
+    });
+
+    // download screenshot
+    $("#download").click(() => {
+        saveDataURI(defaultFileName('.png'), dataURL);
+        $("#download").text(">downloaded");
+        $("#download").removeClass("link");
+    });
+
+    // save screenshot to archive
+    $("#save-archive").click(() => {
+        socket.emit('screenshot', dataURL);
+        $("#save-archive").text(">saved to archive");
+        $("#save-archive").removeClass("link");
+    });
+
+    // exit save screenshot window
+    $("#exit-screenshot").click(() => {
+        $("#save-screenshot").css('display', 'none');
+        $("#save-archive").text(">save to archive");
+        $("#save-archive").addClass("link");
+        $("#download").text(">download");
+        $("#download").addClass("link");
+        active = true;
+        animate();
+    });
+}
+
+function dataURIToBlob(dataURI) {
+    const binStr = window.atob(dataURI.split(',')[1]);
+    const len = binStr.length;
+    const arr = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        arr[i] = binStr.charCodeAt(i);
+    }
+    return new window.Blob([arr]);
+}
+
+function saveDataURI(name, dataURI) {
+    const blob = dataURIToBlob(dataURI);
+
+    // force download
+    const link = document.createElement('a');
+    link.download = name;
+    link.href = window.URL.createObjectURL(blob);
+    link.onclick = () => {
+        window.setTimeout(() => {
+            window.URL.revokeObjectURL(blob);
+            link.removeAttribute('href');
+        }, 500);
+
+    };
+    link.click();
+}
+
+function defaultFileName(ext) {
+    const str = `${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}${ext}`;
+    return str.replace(/\//g, '-').replace(/:/g, '.');
+}
--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const io = socket.listen(server);
-
-let clienthands = [];
+let clients = [];
+let numclients = 0;
+let screenshots = [];
 
 app.use(express.static("public"));
 
@@ -20,30 +21,90 @@ const listener = server.listen(port, () => {
     console.log(`Server is listening on port ${listener.address().port}`);
 });
 
+// HANDLE CONNECTIONS
 io.on("connection", socket => {
     console.log(`New connection: ${socket.id}`);
+    numclients++;
+    socket.emit('numclients', numclients);
+    socket.emit('allscreenshots', screenshots);
 
-    let handposition = {
+    let clientinfo = {
+        num: numclients,
+        name: '',
+        location: '',
         id: socket.id,
         x: -100,
         y: -100
     }
-    clienthands.push(handposition);
 
-    socket.on("handmoved", data => {
-        handposition.x = data.x;
-        handposition.y = data.y;
-
-        io.emit("moving", clienthands);
+    // RECEIVE USER NAME AND LOCATION FROM CLIENT
+    socket.on("userinfo", data => {
+        data.name !== '' ? clientinfo.name = data.name : clientinfo.name = 'an anonymous user';
+        data.location !== '' ? clientinfo.location = data.location : clientinfo.location = 'an unnamed location';
+        socket.broadcast.emit('clientconnected', clientinfo);
     });
 
+    clients.push(clientinfo);
+    let stopDisplaying;
+
+    // RECEIVE HAND POSITION COORDINATES FROM CLIENT
+    socket.on("handmoved", data => {
+
+        // if user inactive for more than 5 seconds, stop displaying
+        clearTimeout(stopDisplaying);
+        stopDisplaying = setTimeout(() => {
+            removeClient(socket.id);
+            console.log(`Stopped displaying ${socket.id} due to inactivity`)
+        }, 5 * 1000);
+        const index = getIndex(socket.id);
+        if (index == -1) {
+            clients.push(clientinfo);
+            console.log(`${socket.id} active again, displaying hand`)
+        }
+
+        // update hand position
+        clientinfo.x = data.x;
+        clientinfo.y = data.y;
+
+        io.emit("moving", clients);
+    });
+
+    // RECEIVE NEW SCREENSHOT FROM CLIENT
+    socket.on('screenshot', data => {
+        console.log("new screenshot received");
+        let audience = [];
+        for (let i = 0; i < clients.length; i++) {
+            let ppl = {
+                name: clients[i].name,
+                location: clients[i].location
+            }
+            audience.push(ppl);
+        }
+        let screenshotinfo = {
+            img: data,
+            people: audience,
+            time: `${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`
+        }
+        screenshots.push(screenshotinfo);
+        io.emit("newscreenshot", screenshotinfo);
+    })
+
+    // HANDLE DISCONNECTIONS
     socket.on("disconnect", () => {
         console.log(`Disconnected: ${socket.id}`);
         removeClient(socket.id);
+        numclients--;
+        clientinfo.num = numclients;
+        socket.broadcast.emit('clientdisconnected', clientinfo);
     });
 });
 
 function removeClient(socket_id) {
-    const index = clienthands.map(e => e.id).indexOf(socket_id);
-    clienthands.splice(index, 1);
+    const index = getIndex(socket_id);
+    clients.splice(index, 1);
+}
+
+function getIndex(socket_id) {
+    const index = clients.map(e => e.id).indexOf(socket_id);
+    return index;
 }
